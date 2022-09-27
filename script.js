293e521837cb91483d7738cddd8b57c0fa878f22
John Seth Salazar <salazarjohnseth@gmail.com>
2022-09-27 21:03:32 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+const hour = document.getElementById('hour');
+const minute = document.getElementById('minute');
+const seconds = document.getElementById('seconds');
+
+const clock = setInterval(
+    function time() {
+        let dateToday = new Date();
+        //console.log(dateToday);
+
+        let hr = dateToday.getHours();
+        let min = dateToday.getMinutes();
+        let sec = dateToday.getSeconds();
+
+        //console.log(hr, min, sec)
+        if(hr<10){
+            hr = '0' + hr;
+        }
+        if(min<10){
+            //min = '0' + min;
+            min = min;
+        }
+        if(sec<10){
+            sec = '0' + sec;
+        }
+
+        //12 hour clock format conversion
+        let ampm = hr >=12 ? 'pm' : 'am';
+        hr = hr % 12;
+        hr = hr ? hr : 12;
+        min = min < 10 ? '0' + min : min;
+        
+        hour.textContent = hr;
+        minute.textContent = min;
+        seconds.textContent = sec;
+}, 1000)
+
+let weather = {
+    "apiKey": "4ed53ec99fdb4fb3c80ba66f81ff2006",
+    fetchWeather: function(city){
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
+            + "&units=metric&appid=" 
+            + this.apiKey
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data){
+        const {name} = data;
+        const {icon, description} = data.weather[0];
+        const {temp, humidity} = data.main;
+        const {speed} = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + "km/h";
+    },
+};
