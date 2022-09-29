63a140b3cfbd3fad44858d471faa55e15fe10b43
John Seth Salazar <salazarjohnseth@gmail.com>
2022-09-29 16:10:34 +0800

    3rd commit

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ let weather = {
         const {icon, description} = data.weather[0];
         const {temp, humidity} = data.main;
         const {speed} = data.wind;
-        console.log(name, icon, description, temp, humidity, speed);
+        //console.log(name, icon, description, temp, humidity, speed);
 
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
@@ -59,6 +59,7 @@ let weather = {
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind Speed: " + speed + "km/h";
+        document.querySelector(".weather").classList.remove("loading");
     },
     search: function(){
         this.fetchWeather(document.querySelector(".search-bar").value);
